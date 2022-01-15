322a59984e7b79dca209d1298d2bcb64a3be8795
WwwwwFeng <wfeng026@ucr.edu>
2022-01-14 22:51:00 -0800

    Fixes #4

--- test.cpp
+++ test.cpp
@@ -1,45 +1,34 @@
 #include "c-echo.h"
-#include "circle.hpp"
+#include "c-count.h"
+
 #include "gtest/gtest.h"
 
 TEST(EchoTest, HelloWorld) {
-	char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "hello"; test_val[2] = "world";
-	EXPECT_EQ("hello world", echo(3,test_val));
+    char* test_val[3]; test_val[0] = "./c-echo-count"; test_val[1] = "hello"; test_val[2] = "world";
+    EXPECT_EQ("hello world", echo(3,test_val));
 }
 
 TEST(EchoTest, EmptyString) {
-	char* test_val[1]; test_val[0] = "./c-echo";
-	EXPECT_EQ("", echo(1,test_val));
+    char* test_val[1]; test_val[0] = "./c-echo-count";
+    EXPECT_EQ("", echo(1, test_val));
 }
 
-TEST(EchoTest, HaveANiceDay) {
-        char* test_val[5]; test_val[0] = "./c-echo"; test_val[1] = "have"; test_val[2] = "a"; test_val[3] = "nice"; test_val[4] = "day";
-        EXPECT_EQ("have a nice day", echo(5,test_val));
+TEST(CountTest, HelloWorld) {
+    std::string test_str = "hello world";
+    EXPECT_EQ(2, count(test_str));
 }
 
-TEST(EchoTest, GoodJob) {
-        char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "good"; test_val[2] = "job";
-        EXPECT_EQ("good job", echo(3,test_val));
+TEST(CountTest, EmptyString) {
+    std::string test_str = "";
+    EXPECT_EQ(0, count(test_str));
 }
 
-TEST(EchoTest, TakeCare) {
-        char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "take"; test_val[2] = "care";
-        EXPECT_EQ("take care", echo(3,test_val));
-}
-
-TEST(Area, CanComputeOneArea) {
-	Circle c1 = Circle(9);
-	double res = c1.area();
-	EXPECT_DOUBLE_EQ(res, 254.46900494077323);
-}
-
-TEST(Perimeter, CanComputeOnePerimeter) {
-	Circle c2 = Circle(5);
-	double res = c2.perimeter();
-	EXPECT_DOUBLE_EQ(res, 31.415926535897931);
+TEST(CountTest, ManySpaces) {
+    std::string test_str = "   this   string has     weird   spacing";
+    EXPECT_EQ(5, count(test_str));
 }
 
 int main(int argc, char **argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
