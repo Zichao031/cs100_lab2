ebe387c987013748c6d885205921d8d7f0821197
WwwwwFeng <wfeng026@ucr.edu>
2022-01-15 15:35:46 -0800

    Changed c-echo.h

--- c-echo.h
+++ c-echo.h
@@ -1,11 +1,6 @@
-#include <iostream>
-std::string echo(int length, char** chars) {
-	std::string ret = "";
-	for(int i = 1; i < length; i++){
-		ret += chars[i];
-		if(i < length - 1){
-			ret += " ";
-		}
-	}
-	return ret;
+#include "c-echo.h"
+#include "c-count.h"
+
+int main(int argv, char** argc) {
+    std::cout << "Count of output: " << count(echo(argv,argc)) << std::endl;
 }
